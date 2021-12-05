05f592138a1a271c318c68ce10a2da1f60b071dc
Евгений Плаксин <isip_e.e.plaksin@mpt.ru>
2021-12-05 17:28:55 +0300

    Sensor

---
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.sensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sysmanager;
+    private Sensor sensor;
+    private ImageView img;
+    private TextView txt;
+    private SensorEventListener sv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.txt);
+        img = findViewById(R.id.img);
+        sysmanager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(sysmanager != null)
+            sensor = sysmanager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        sv = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, sensorEvent.values);
+                float[] remappedRotationMatrix = new float[16];
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z,
+                        remappedRotationMatrix);
+                float[] orientations = new float[3];
+                SensorManager.getOrientation(remappedRotationMatrix, orientations);
+                for(int i = 0; i < 3; i++){
+                    orientations[i] = (float)(Math.toDegrees(orientations[i]));
+                }
+                txt.setText(String.valueOf((int)orientations[2]));
+                img.setRotation(-orientations[2]);
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {}
+        };
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sysmanager.registerListener(sv,sensor,SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sysmanager.unregisterListener(sv);
+    }
+}
